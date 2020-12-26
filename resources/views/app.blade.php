3750b83665d61b95bd0055b44d0a056c160233fc
Md Nazmul Hossain <annazmul71@gmail.com>
2020-12-26 21:42:18 +0600

    Scaffholding Publishing and App Logo, Favicon Setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('AskMe.png') }}" type="image/x-icon">
+
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
